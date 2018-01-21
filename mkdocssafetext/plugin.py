2385971011fe10f33093f9d95d66bcecd252882f
raimon <raimon49@hotmail.com>
2018-01-22 00:37:07 +0900

    Trying to search own configuration values

--- mkdocssafetext/plugin.py
+++ mkdocssafetext/plugin.py
@@ -2,6 +2,7 @@
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 from __future__ import (division, print_function,
                         absolute_import, unicode_literals)
+import copy
 
 from mkdocs.plugins import BasePlugin
 import bleach
@@ -10,9 +11,14 @@ from bleach_whitelist import markdown_tags, markdown_attrs
 
 class SafeTextPlugin(BasePlugin):
     def __init__(self):
-        pass
+        self.markdown_tags = copy.deepcopy(markdown_tags)
+        self.markdown_attrs = copy.deepcopy(markdown_attrs)
 
     def on_config(self, config):
+        for cfg in config.user_configs:
+            if isinstance(cfg, dict) and 'plugins' in cfg:
+                self.plugin_config = cfg['plugins']
+
         return config
 
     def on_page_markdown(self, markdown, page, config, site_navigation):
@@ -20,8 +26,8 @@ class SafeTextPlugin(BasePlugin):
 
     def on_page_content(self, html, page, config, site_navigation):
         return bleach.clean(html,
-                            tags=markdown_tags,
-                            attributes=markdown_attrs)
+                            tags=self.markdown_tags,
+                            attributes=self.markdown_attrs)
 
     def on_page_context(self, context, page, config, site_navigation):
         return context
