25293c4205d94d68fff2a7168ceb02176a99c93d
raimon <raimon49@hotmail.com>
2018-09-06 23:38:28 +0900

    Add spec of requirements Python version

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
     include_package_data=True,
     keywords='mkdocs bleach xss',
     license=LICENSE,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=[
         'bleach',
         'bleach-whitelist',
